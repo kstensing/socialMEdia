b2d0f48fea5df20555a52b8e3ab7578d4bdffe62
kstensing <stensingk@gmail.com>
2022-04-23 17:01:18 -0500

    user routes

--- models/User.js
+++ models/User.js
@@ -2,43 +2,49 @@ const {
     Schema,
     model
 } = require('mongoose');
+const {
+    stringify
+} = require('querystring');
 //const dateFormat = require('../utils/dateFormat');
 
 const UserSchema = new Schema({
-            username: {
-                type: String,
-                unique: true,
-                required: true,
-                trim: true
-            },
-            email: {
-                type: String,
-                required: true,
-                unique: true,
-                match: [/.+@.+\..+/]
-            },
-            thoughts: {
-                type: Date,
-                default: Date.now,
-                get: createdAtVal => dateFormat(createdAtVal)
-            },
-            friends: [],
-            // comments: [
-        //     {
-        //         type: Schema.Types.ObjectId,
-        //         ref: 'Comment'
-        //     }
-        // ]
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
     },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        // the id false below prevents virtuals from making a duplicate _id as `id`
-        id: false
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
     }
-);
+    ],
+    friends: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    // comments: [
+    //     {
+    //         type: Schema.Types.ObjectId,
+    //         ref: 'Comment'
+    //     }
+    // ]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    // the id false below prevents virtuals from making a duplicate _id as `id`
+    id: false
+});
 
 UserSchema.virtual('friendCount').get(function () {
     return this.comments.reduce(
